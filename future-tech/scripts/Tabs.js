0242ede72dfa178639c63cc9c7a73503401d2299
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-03-18 18:38:21 +0300

    main content. JS модуль Tabs. созданы поля класса Tabs

--- future-tech/scripts/Tabs.js
+++ future-tech/scripts/Tabs.js
@@ -1,10 +1,39 @@
+const rootSelector = '[data-js-tabs]'
+
 // основная логика конкретной группы табов
 class Tabs {
+    selectors = {
+        root: rootSelector,
+        button: '[data-js-tab-button]',
+        content: '[data-js-tabs-content]',
+    }
+
+    stateClasses = {
+        isActive: 'is-active',
+    }
+
+    stateAttributes = {
+        ariaSelected: 'aria-selected',
+        tabIndex: 'tabindex',
+    }
+
+
+
 }
 
 
 // инициализация логики всех табов на одной странице
 class TabsCollection {
+    constructor() {
+        this.init()
+    }
+
+    init() {
+        document.querySelectorAll(rootSelector).forEach((element) => {
+            new Tabs(element)
+        })
+
+    }
 }
 
 export default TabsCollection
