afc9e508798c5f53961ec656eb78a589293db99f
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-03-18 19:15:27 +0300

    main content. JS модуль Tabs. созданы методы класса Tabs

--- future-tech/scripts/Tabs.js
+++ future-tech/scripts/Tabs.js
@@ -2,6 +2,7 @@ const rootSelector = '[data-js-tabs]'
 
 // основная логика конкретной группы табов
 class Tabs {
+    // создаем поля класса
     selectors = {
         root: rootSelector,
         button: '[data-js-tab-button]',
@@ -17,10 +18,48 @@ class Tabs {
         tabIndex: 'tabindex',
     }
 
+    // создаем методы класса
+    constructor(rootElement) {
+        this.rootElement = rootElement;
+        this.buttonElements = this.rootElement.querySelectorAll(this.selectors.button);
+        this.contentElements = this.rootElement.querySelectorAll(this.selectors.content);
+        this.state = {
+            activeTabIndex: [...this.buttonElements]
+                .findIndex((buttonElement) => buttonElement.classList.contains(this.stateClasses.isActive)),
+        }
+        this.limitTabsIndex = this.buttonElements.length - 1;
+        this.bindEvents()
+    }
 
+    updateUI() {
+        const {activeTabIndex} = this.state
 
+        this.buttonElements.forEach((buttonElement, index) => {
+            const isActive = index === activeTabIndex
+
+            buttonElement.classList.toggle(this.stateClasses.isActive, isActive)
+        })
+
+        this.contentElements.forEach((contentElement, index) => {
+            const isActive = index === activeTabIndex
+
+            contentElement.classList.toggle(this.stateClasses.isActive, isActive)
+        })
+    }
+
+    onButtonClick(buttonIndex) {
+        this.state.activeTabIndex = buttonIndex
+        this.updateUI()
+    }
+
+    bindEvents() {
+        this.buttonElements.forEach((buttonElement, index) => {
+            buttonElement.addEventListener('click', () => {
+                this.onButtonClick(index)
+            })
+        })
+    }
 }
-
 
 // инициализация логики всех табов на одной странице
 class TabsCollection {
